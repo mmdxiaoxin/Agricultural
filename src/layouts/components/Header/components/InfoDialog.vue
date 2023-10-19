67b13f50d1c4798bfe3024cca77f7b02523e9fbe
小新 <782446723@qq.com>
2023-10-19 20:20:05 +0800

    增加用户个人详细信息Dialog

--- src/layouts/components/Header/components/InfoDialog.vue
+++ src/layouts/components/Header/components/InfoDialog.vue
@@ -1,6 +1,14 @@
 <template>
   <el-dialog v-model="dialogVisible" title="个人信息" width="500px" draggable>
-    <span>This is userInfo</span>
+    <div>
+      <h3>用户信息</h3>
+      <p><strong>用户名:</strong> {{ userInfo.username }}</p>
+      <p><strong>性别:</strong> {{ userInfo.gender }}</p>
+      <p><strong>身份证号:</strong> {{ userInfo.idCard }}</p>
+      <p><strong>邮箱:</strong> {{ userInfo.email }}</p>
+      <p><strong>地址:</strong> {{ userInfo.address }}</p>
+      <p><strong>创建时间:</strong> {{ userInfo.createTime }}</p>
+    </div>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
@@ -12,8 +20,17 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { User } from "@/api/interface";
 
 const dialogVisible = ref(false);
+const userInfo = ref<User.ResUserList>({
+  username: "小新AI",
+  gender: "男",
+  idCard: "123456789012345678",
+  email: "xxxxxx@qq.com",
+  address: "xx省xx市xx区",
+  createTime: "2021-08-01 12:00:00"
+});
 const openDialog = () => {
   dialogVisible.value = true;
 };

cb4c16363cb5783a3288a13650b510de4c7723a2
小新 <782446723@qq.com>
2023-11-19 10:12:16 +0800

    美化个人信息对话框。

--- src/layouts/components/Header/components/InfoDialog.vue
+++ src/layouts/components/Header/components/InfoDialog.vue
@@ -1,39 +1,88 @@
 <template>
-  <el-dialog v-model="dialogVisible" title="个人信息" width="500px" draggable>
-    <div>
-      <h3>用户信息</h3>
-      <p><strong>用户名:</strong> {{ userInfo.username }}</p>
-      <p><strong>性别:</strong> {{ userInfo.gender }}</p>
-      <p><strong>身份证号:</strong> {{ userInfo.idCard }}</p>
-      <p><strong>邮箱:</strong> {{ userInfo.email }}</p>
-      <p><strong>地址:</strong> {{ userInfo.address }}</p>
-      <p><strong>创建时间:</strong> {{ userInfo.createTime }}</p>
+  <el-dialog v-model="dialogVisible" title="个人信息" min-width="500px" draggable>
+    <div class="el-dialog--center">
+      <el-page-header @back="closeDialog">
+        <template #content>
+          <div class="items-center">
+            <el-avatar class="mr-3" :size="32" :src="useStore.userInfo.avatar" />
+            <span style="margin-left: 20px" v-if="!editMode">{{ useStore.userInfo.name }}</span>
+            <el-input v-else v-model="editedUserInfo.name" style="max-width: 30%; margin-left: 20px" />
+            <el-tag style="margin-left: 20px" :type="getTagType(useStore.userInfo.currentRole)">
+              {{ useStore.userInfo.currentRole }}
+            </el-tag>
+          </div>
+        </template>
+        <template #extra>
+          <div class="items-center">
+            <el-button :type="editMode ? 'info' : 'primary'" @click="toggleEditMode">
+              {{ editMode ? "保存" : "编辑" }}
+            </el-button>
+          </div>
+        </template>
+        <el-descriptions :column="1" border>
+          <el-descriptions-item label="邮箱">
+            <span v-if="!editMode">{{ useStore.userInfo.email }}</span>
+            <el-input v-else v-model="editedUserInfo.email" />
+          </el-descriptions-item>
+          <el-descriptions-item label="手机号">
+            <span v-if="!editMode">{{ useStore.userInfo.phone }}</span>
+            <el-input v-else v-model="editedUserInfo.phone" />
+          </el-descriptions-item>
+          <el-descriptions-item label="个人简介" class="descriptions">
+            <span v-if="!editMode">{{ useStore.userInfo.description }}</span>
+            <el-input v-else v-model="editedUserInfo.description" />
+          </el-descriptions-item>
+        </el-descriptions>
+      </el-page-header>
     </div>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
-      </span>
-    </template>
   </el-dialog>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { User } from "@/api/interface";
+import { useUserStore } from "@/stores/modules/user";
+import { getTagType } from "@/utils/workPlace";
 
+const useStore = useUserStore();
 const dialogVisible = ref(false);
-const userInfo = ref<User.ResUserList>({
-  username: "小新AI",
-  gender: "男",
-  idCard: "123456789012345678",
-  email: "xxxxxx@qq.com",
-  address: "xx省xx市xx区",
-  createTime: "2021-08-01 12:00:00"
+const editMode = ref(false);
+
+const editedUserInfo = ref({
+  name: useStore.userInfo.name,
+  email: useStore.userInfo.email,
+  phone: useStore.userInfo.phone,
+  description: useStore.userInfo.description
 });
+
 const openDialog = () => {
   dialogVisible.value = true;
 };
 
+const closeDialog = () => {
+  dialogVisible.value = false;
+  editMode.value = false;
+};
+
+const toggleEditMode = () => {
+  if (editMode.value) {
+    // 保存修改后的个人信息，这里可以进行保存逻辑
+    // 例如，将editedUserInfo的值提交到后端
+  }
+  editMode.value = !editMode.value;
+};
+
 defineExpose({ openDialog });
 </script>
+
+<style scoped lang="scss">
+.items-center {
+  display: flex;
+  align-items: center;
+
+  .descriptions {
+    width: 100%;
+    border: #575757 1px solid;
+    min-height: 250px;
+  }
+}
+</style>
--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -4,9 +4,19 @@ import piniaPersistConfig from "@/config/piniaPersist";
 
 export const useUserStore = defineStore({
   id: "agricultural-user",
-  state: (): UserState => ({
+  state: (): {
+    userInfo: { phone: string; name: string; description: string; avatar: string; email: string; currentRole: string };
+    token: string;
+  } => ({
     token: "",
-    userInfo: { name: "小新AI" }
+    userInfo: {
+      name: "小新AI",
+      email: "782446723@qq.com",
+      phone: "18888888888",
+      avatar: "src/assets/images/avatar.png",
+      description: "嗨嗨嗨，测试",
+      currentRole: "管理员"
+    }
   }),
   getters: {},
   actions: {
